6b79939de67c674c5990263a0635f615c07fc5c7
Andrey Vasilyev <aav_55@mail.ru>
2023-12-07 21:11:21 +0300

    fix: build

--- jenkins/scripts/npm-hosted.sh
+++ jenkins/scripts/npm-hosted.sh
@@ -10,7 +10,7 @@
 npm config set registry http://192.168.1.39:8081/repository/npm-hosted/
 npm config set _auth YWRtaW46MTIzCg==
 #npm publish
-npm publish --registry http://192.168.1.39:8081/repository/npm-hosted/
+npm publish ./ --registry=http://192.168.1.39:8081/repository/npm-hosted/ --non-interactive --verbose
 
 
 #npm install -g npm-cli-login
