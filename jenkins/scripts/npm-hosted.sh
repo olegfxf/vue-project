16541f764f0b3d76ac6fe96ab11c36e63d73ed48
Andrey Vasilyev <aav_55@mail.ru>
2023-12-07 17:51:50 +0300

    fix: build

--- jenkins/scripts/npm-hosted.sh
+++ jenkins/scripts/npm-hosted.sh
@@ -2,6 +2,13 @@
 
 #npm login -u admin -p 123 127.0.0.1:8081
 
+#npm config set registry http://192.168.1.39:8081/repository/npm-hosted/
+#npm-cli-login -u admin -p 123 -e nick@foo.bar -r http://192.168.1.39:8081/repository/npm-hosted/
+#npm publish
+
+
+npm config set registry http://192.168.1.39:8081/repository/npm-images/
+npm install -g npm-cli-login
 npm config set registry http://192.168.1.39:8081/repository/npm-hosted/
 npm-cli-login -u admin -p 123 -e nick@foo.bar -r http://192.168.1.39:8081/repository/npm-hosted/
-npm publish
+npm publish registry=http://192.168.1.39:8081/repository/npm-hosted/
