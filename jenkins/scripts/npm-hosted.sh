2969fc041683529454dc2c9b62617c0aaf708aff
Andrey Vasilyev <aav_55@mail.ru>
2023-12-07 17:35:37 +0300

    fix: build

--- jenkins/scripts/npm-hosted.sh
+++ jenkins/scripts/npm-hosted.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 
-#npm config set registry http://192.168.1.39:8081/repository/npm-hosted/
 #npm login -u admin -p 123 127.0.0.1:8081
-#npm publish
 
 npm config set registry http://192.168.1.39:8081/repository/npm-hosted/
 npm-cli-login -u admin -p 123 -e nick@foo.bar -r http://192.168.1.39:8081/repository/npm-hosted/
