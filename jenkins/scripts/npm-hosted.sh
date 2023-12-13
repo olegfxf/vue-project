61b99ade98b42e327505a88bb90ea6352241ae0d
Andrey Vasilyev <aav_55@mail.ru>
2023-12-13 18:38:48 +0300

    fix: build

--- jenkins/scripts/npm-hosted.sh
+++ jenkins/scripts/npm-hosted.sh
@@ -6,6 +6,7 @@ echo $1
 npm version $1
 npm pack
 export paramF=npm.asset=@vue-project-$1.tgz\;type=application/x-compressed
+echo $paramF
 curl -u admin:123 -X POST "http://localhost:8081/service/rest/v1/components?repository=npm-hosted" -H "accept: application/json" -H "Content-Type: multipart/form-data" -F $paramF
 
 
